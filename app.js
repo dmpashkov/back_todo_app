97d5eeaa04d888f885bf014ec124d639f64e1c21
Dmitry Pashkov <pashkov.fortech@gmail.com>
2022-06-10 17:57:51 +0300

    implemented integration with mongodb, add task and delete task

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 
 const apiRoutes = require("./src/modules/routes/routes");
 
 app.use(cors());
+
+const uri = "mongodb+srv://pashkov_fortech:Pashkov123321@cluster0.vgubrb7.mongodb.net/todo_app?retryWrites=true&w=majority"
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.json());
 app.use("/", apiRoutes);
--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -1,47 +1,32 @@
-const tasks = [];
+const Task = require('../../db/models/task/index');
 
 module.exports.getAllTasks = async (req, res, next) => {
-  res.send({data: tasks});
+  Task.find().then(result => {
+    res.send({data: result});
+  });
 };
 
 module.exports.createNewTask = (req, res, next) => {
-  const body = req.body;
-  if (body.hasOwnProperty('text') && body.hasOwnProperty('isCheck')) {
-    body.id = Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
-    tasks.push(body);
-    res.send({data: tasks});
-  } else {
-    res.status(422).send('Error! Params not correct');
-  }
+  const task = new Task(req.body);
+  console.log(req.body);
+  task.save().then(result => {
+    res.send(result);
+  }).catch(err => console.log(err));
+  console.log(req.body);
 };
 
 module.exports.changeTaskInfo = (req, res, next) => {
-  const body = req.body;
-  if (body.hasOwnProperty('id') && (body.hasOwnProperty('text') || body.hasOwnProperty('isCheck'))) {
-    tasks.forEach((item, i) => {
-      if(item.id === body.id) {
-        for(let key in body) {
-          tasks[i][key] = body[key];
-        }
-      }
+  Task.updateOne({"_id": req.body._id}, req.body).then(result => {
+    Task.find({"_id": req.body._id}).then(result => {
+      res.send(result);
+      console.log(req.body);
     });
-    res.send({data: tasks});
-  } else {
-    res.status(422).send('Error! Params not correct');
-  }
+  });
 };
 
 module.exports.deleteTask = (req, res, next) => {
-  if (!req.query.id) return res.status(422).send('Error! Params not correct');
-  const task = tasks.filter(item => item.id === req.query.id);
-  if (task.length) {
-    tasks.forEach((item, i) => {
-      if(item.id === req.query.id) {
-        tasks.splice(i, 1);
-      }
-    });
-    res.send({data: tasks});
-  } else {
-    res.status(404).send('Task not found');
-  }
+  Task.deleteOne({_id: req.query._id}).then(result => {
+    console.log(req.query);
+    res.send(result);
+  });
 };
